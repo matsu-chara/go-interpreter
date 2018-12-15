74bc917e6dbf5257b17a0e924758bda1ff72200b
matsu-chara <matsuy00@gmail.com>
2018-12-15 23:59:52 +0900

    read eval print loop

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/matsu-chara/go-interpreter/monkey/evaluator"
 	"github.com/matsu-chara/go-interpreter/monkey/lexer"
 	"github.com/matsu-chara/go-interpreter/monkey/parser"
 )
@@ -34,8 +35,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
