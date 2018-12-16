5f9e38701b78709aa7cb6ca34e19e420a0894075
matsu-chara <matsuy00@gmail.com>
2018-12-16 17:17:41 +0900

    string lexer

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -57,6 +57,8 @@ if (5 < 10) {
 
 10 == 10;
 10 != 9;
+"foobar"
+"foo bar"
 `
 
 	tests := []struct {
@@ -136,6 +138,8 @@ if (5 < 10) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
