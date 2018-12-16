e16db2165c3915420d6cafad7190a54dffbd01fc
matsu-chara <matsuy00@gmail.com>
2018-12-16 16:25:46 +0900

    identifier by env

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -57,12 +57,14 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return val
 		}
 		return &object.ReturnValue{Value: val}
-
 	case *ast.LetStatement:
 		val := Eval(node.Value, env)
 		if isError(val) {
 			return val
 		}
+		env.Set(node.Name.Value, val)
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
 	}
 	return nil
 }
@@ -213,4 +215,13 @@ func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) obje
 
 func newError(format string, a ...interface{}) *object.Error {
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
+	val, ok := env.Get(node.Value)
+	if !ok {
+		return newError("identifier not found: " + node.Value)
+	}
+
+	return val
 }
