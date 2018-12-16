53670491e23cc2f8cd61a2a1abf90deb1564c631
matsu-chara <matsuy00@gmail.com>
2018-12-16 16:23:43 +0900

    add environment

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/matsu-chara/go-interpreter/monkey/object"
+
 	"github.com/matsu-chara/go-interpreter/monkey/evaluator"
 	"github.com/matsu-chara/go-interpreter/monkey/lexer"
 	"github.com/matsu-chara/go-interpreter/monkey/parser"
@@ -14,6 +16,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -35,7 +38,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
